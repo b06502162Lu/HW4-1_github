bb893e5dd8f6c0e14586b0400b0b107edef73c02
Liang-Yu Lu <liang-yulu@Liang-Yus-MacBook-Pro.local>
2020-10-14 10:08:33 +0800

    test

--- bert/similarity_student.py
+++ bert/similarity_student.py
@@ -106,7 +106,7 @@ def Anisotropy(data,layer_index,version):
 # Question 2
 def cosine_similarity_Anisotropy(two_words):
     print("This is a test : ",len(two_words))
-    print("test_size : ",len(two_words[0]),"   ||   ",len(two_words[1].size()),"  ||   ",type(two_words[0]))
+    print("test_size : ",len(two_words[0]),"   ||   ",len(two_words[1]),"  ||   ",type(two_words[0]))
     cos = None
     """
     Todo: return two word cosine similarity
